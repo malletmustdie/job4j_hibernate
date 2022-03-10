3f27971a86742dbf00d58d776dae00a79571724e
Илья Шокин <mallet322@gmail.com>
2022-03-10 15:37:20 +0300

    0. HQL [#6874]

---
+++ src/main/java/ru/job4j/hibernate/candidates/Candidate.java
@@ -0,0 +1,84 @@
+package ru.job4j.hibernate.candidates;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer experience;
+
+    private Double salary;
+
+    public Candidate() {
+    }
+
+    public static Candidate of(String name, Integer experience, Double salary) {
+        Candidate candidate = new Candidate();
+        candidate.name = name;
+        candidate.experience = experience;
+        candidate.salary = salary;
+        return candidate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id.equals(candidate.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Candidate: id=%s, name=%s, experience=%d, salary=%f",
+                             id,
+                             name,
+                             experience,
+                             salary);
+    }
+
+}
---
+++ src/main/java/ru/job4j/hibernate/candidates/HbmRun.java
@@ -0,0 +1,73 @@
+package ru.job4j.hibernate.candidates;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HbmRun {
+
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry =
+                new StandardServiceRegistryBuilder().configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Candidate one = Candidate.of("Alex", 1, 90000.0);
+            Candidate two = Candidate.of("Michael", 3, 190000.0);
+            Candidate three = Candidate.of("Jim", 10, 999999.999);
+
+            session.save(one);
+            session.save(two);
+            session.save(three);
+
+            List<Candidate> candidates = session.createQuery("from Candidate c")
+                                                .list();
+            candidates.forEach(c -> System.out.println(c.getName() + " - " + c.getSalary()));
+
+            Candidate candidateFoundedById =
+                    (Candidate) session.createQuery(
+                                               "from Candidate c "
+                                                       + "where c.id = :fId")
+                                       .setParameter("fId", 1L)
+                                       .uniqueResult();
+            System.out.println(candidateFoundedById);
+
+            Candidate candidateFoundedByName =
+                    (Candidate) session.createQuery(
+                                               "from Candidate c "
+                                                       + "where c.name = :name")
+                                       .setParameter("name", "Jim")
+                                       .uniqueResult();
+            System.out.println(candidateFoundedByName);
+
+            session.createQuery("update Candidate c "
+                                        + "set c.name = :name, "
+                                        + "c.experience = :experience, "
+                                        + "c.salary = :salary "
+                                        + "where c.id = :fId")
+                   .setParameter("name", "Dwightttt")
+                   .setParameter("experience", 5)
+                   .setParameter("salary", 500000.00)
+                   .setParameter("fId", 1L)
+                   .executeUpdate();
+
+            session.createQuery("delete from Candidate c where c.id = :fId")
+                   .setParameter("fId", 7L)
+                   .executeUpdate();
+
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+}
